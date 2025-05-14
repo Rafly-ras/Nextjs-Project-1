0b05946e6b41bdf64c566b187051f76061a21835
unknown <adityarafly504@gmail.com>
2025-05-14 10:15:06 +0700

    menambahkan navbar dan homepage

--- app/page.js
+++ app/page.js
@@ -1,7 +1,36 @@
+import Image from "next/image";
+import DataImage from '@/public/data';
+import HeroInfo from "@/components/HeroInfo";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Homepage</h1>
-    </div>
+    <>
+      {/* Title*/}
+      <div className="py-20">
+        <h1 className="sm:text-6xl/tight text-5xl/tight sm:text-center text-left">
+          Tingkatkan Gaya Hidup Anda <br /> Bersama Kami
+        </h1>
+      </div>
+      {/* Title*/}
+
+      {/* Hero Image*/}
+      <div className="max-w-5xl mx-auto relative">
+        <Image src={DataImage.Hero} alt="Hero Image" priority/>
+
+        <HeroInfo />
+      </div>
+      {/* Hero Image*/}
+
+      {/* Layanan */}
+      <div>
+        <div>
+          <i className="ri-money-dollar-circle-fill"></i>
+          <p>Memberikan Harga Terbaik</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi aspernatur, 
+            odio quibusdam a repellendus velit?</p>
+        </div>
+      </div>
+      {/* Layanan */}
+    </>
   );
 }
